e0dcae9ad3c250ded0430f00d4cb0726376aa3c7
Yamilka <Yamilka_268465460@hotmail.com>
2021-07-05 18:16:38 -0400

    I do the Anagram method

--- KataGrupal/Program.cs
+++ KataGrupal/Program.cs
@@ -9,9 +9,49 @@ namespace KataGrupal
         {
             public Hashtable ListAnagrams;
             public Hashtable Index;
-            public void FindAnagrams(string[] path)
+           
+           public string[] readTxt(string path){
+
+            string[] Words = new string[];
+            int i = 0;
+                while ((line = file.ReadLine()) != null)
+                {
+                    Words[i] = line;
+                    i++;
+                }
+
+           }
+           
+           
+            public void FindAnagrams(string[] words)
             {
                 
+                ListAnagrams = new Hastable();
+                Index = new Hastable();
+
+                for (int i = 0; i < words; i++)
+                {
+
+                    //Ordenar la palabra
+                    string Word = new String(words[i].OrderBy(x => x).ToArray());
+                    
+                    //Verificar si la palabra se encuentra en el hashtable
+                    if (ListAnagrams.ContainsKey(Word))
+                    {
+                        //concatenar la palabra sin ordenar al grupo de anagramas
+                        ListAnagrams[Word] = (ListAnagrams[Word] + ", " + words[i]);
+
+                        //si la palabra no se encuentra en INDEX se agrega para tener
+                        //una lista de las palagras que si son anagramas
+                        if (!Index.ContainsKey(Word)) {
+                            Index.Add(Word, Word);
+                        }
+                    }else
+                    {
+                        ListAnagrams.Add(Word, Words[i]);
+                    }
+                }
+                }
             }
         }
 
