f1280153ef2f86ac73bd89b1fec6b220ceebae60
Yamilka <Yamilka_268465460@hotmail.com>
2021-07-05 20:30:11 -0400

    COCO

--- KataGrupal/Program.cs
+++ KataGrupal/Program.cs
@@ -12,65 +12,58 @@ namespace KataGrupal
             public Hashtable ListAnagrams;
             public Hashtable Index;
            
-           public List<string> readTxt(string path){
-
-            string line;
-            System.IO.StreamReader file = new System.IO.StreamReader(path);
-            List<string> Words = new List<string>();
-            int o = 0;
+            public List<string> readTxt(string path)
+            {
+                string line;
+                System.IO.StreamReader file = new System.IO.StreamReader(path);
+                List<string> Words = new List<string>();
+                int o = 0;
+            
                 while ((line = file.ReadLine()) != null)
                 {
                     Words[o] = line;
                     o++;
                 }
-            return Words;
+                return Words;
            }
            
            
             public void FindAnagrams(string[] words)
-            {
-                
+            {  
                 ListAnagrams = new Hashtable();
                 Index = new Hashtable();
 
                 for (int i = 0; i < words.Count(); i++)
                 {
-
-                    //Ordenar la palabra
                     string Word = new String(words[i].OrderBy(x => x).ToArray());
 
-                    //Verificar si la palabra se encuentra en el hashtable
                     if (ListAnagrams.ContainsKey(Word))
                     {
-                        //concatenar la palabra sin ordenar al grupo de anagramas
                         ListAnagrams[Word] = (ListAnagrams[Word] + ", " + words[i]);
 
-                        //si la palabra no se encuentra en INDEX se agrega para tener
-                        //una lista de las palagras que si son anagramas
-                        if (!Index.ContainsKey(Word)) {
+                        if (!Index.ContainsKey(Word)) 
+                        {
                             Index.Add(Word, Word);
                         }
-                    }else
+                    }
+                    else
                     {
                         ListAnagrams.Add(Word, words[i]);
                     }
                 }
                 
-                 //IMPRIMIR LA LISTA DE ANAGRAMAS EN BASE A *INDEX*
+                 //IMPRIMIR
                 int a = 0;
                 foreach (DictionaryEntry item in Index)
-                 {
+                {
                     a++;
-                     Console.WriteLine(ListAnagrams[item.Value]);
-                 }
-                Console.WriteLine("cantidad " + a);
-        }
+                    Console.WriteLine(ListAnagrams[item.Value]);
+                }
+                ConsoleWriteLine("cantidad " + a);
             }
+        }
             
         
-       
-        
-
 
         static void Main(string[] args)
         {
--- KataGrupalTest/UnitTest1.cs
+++ KataGrupalTest/UnitTest1.cs
@@ -2,7 +2,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using KataGrupal;
 using System;
 
-
 namespace AnagramTest
 {
     [TestClass]
@@ -13,12 +12,10 @@ namespace AnagramTest
         {
             var instance = new Program.Anagram();
             string[] Words = {"amor","roma","moro","",""};
-        
 
             DateTime startTime= DateTime.Now;
             instance.FindAnagrams(Words);
             TimeSpan result = (DateTime.Now - startTime);
-
 
             TimeSpan Test = new TimeSpan(0, 0, 0, 2);
             Assert.IsTrue(TimeSpan.Compare(result, Test) < 1);
